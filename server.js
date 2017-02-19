fbfce7091aaaaf8d91d6c42aac8e17ddbd7b3742
Aviv <avivwei@gmail.com>
2017-02-19 16:39:30 +0200

    saving to db

--- server.js
+++ server.js
@@ -1,9 +1,27 @@
 const express = require('express');
+const bodyParser = require('body-parser');
 const app = express();
+const mongoose = require('mongoose');
+const Quote = require('./QuoteModel')
+
+mongoose.connect('mongodb://localhost/quotes');
+
+app.use(bodyParser.urlencoded({extende: true}));
+
 
 app.get('/', (req,res) => {
-  res.sendfile(__dirname + '/index.html');
-  
+  res.sendfile(__dirname + '/index.html');  
+})
+
+app.post('/', (req,res,next) => {
+  const quote = new Quote(req.body);
+  console.log(req.body)
+  quote.save((err,quote) => {
+    if (err){return next(err);}
+    console.log(quote);
+    res.json(quote);
+  });
+
 })
 
 app.listen(3000, function(){
